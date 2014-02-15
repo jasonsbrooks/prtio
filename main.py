f5911cbcf3d9b4ec1660a9499b64c18cf73e4e04
jasonsbrooks <jason.brooks@yale.edu>
2014-02-15 00:02:53 -0500

    Set up database

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ app setup thinkful.com.
 
 
 from flask import Flask
-# from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-# app.debug = env("DEBUG", False, False) in ('True', 'true')
-# app.config['SQLALCHEMY_DATABASE_URI'] = env('DATABASE_URL')
-# db = SQLAlchemy(app)
+app.debug = env("DEBUG", False, False) in ('True', 'true')
+app.config['SQLALCHEMY_DATABASE_URI'] = env('DATABASE_URL')
+db = SQLAlchemy(app)
 # for flaskext.auth -- secret key needed to use sessions
-# app.secret_key = env('USER_AUTH_SECRET_KEY')
+app.secret_key = env('USER_AUTH_SECRET_KEY')
