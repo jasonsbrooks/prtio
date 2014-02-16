76eebeae667a94d7ea5b07917e443e1449b2f3f1
jasonsbrooks <jason.brooks@yale.edu>
2014-02-16 05:46:43 -0500

    Changed license keys for rdio

--- party/static/js/token.js
+++ party/static/js/token.js
@@ -1,4 +1,4 @@
-// var playback_token="GBNTAGHX_____2R2cHlzNHd5ZXg3Z2M0OXdoaDY3aHdrbnBydGlvLmhlcm9rdWFwcC5jb22dqXykEPibtPJA2xLVrlI7"
-// var domain="prtio.herokuapp.com"
-var playback_token="GAlTAGcx_____2R2cHlzNHd5ZXg3Z2M0OXdoaDY3aHdrbmxvY2FsaG9zdHetiaTE5puOeD8O9dLBHWg="
-var domain="localhost"
+var playback_token="GBNTAGHX_____2R2cHlzNHd5ZXg3Z2M0OXdoaDY3aHdrbnBydGlvLmhlcm9rdWFwcC5jb22dqXykEPibtPJA2xLVrlI7"
+var domain="prtio.herokuapp.com"
+// var playback_token="GAlTAGcx_____2R2cHlzNHd5ZXg3Z2M0OXdoaDY3aHdrbmxvY2FsaG9zdHetiaTE5puOeD8O9dLBHWg="
+// var domain="localhost"
